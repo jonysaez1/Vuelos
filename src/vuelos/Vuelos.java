dd915fa5127abb1bdcbabacf9413ef77feeeab1c
Jonathan Saez <jonysaez1@gmail.com>
2018-09-24 11:45:21 -0300

    primer commit de vuelos

---
+++ src/vuelos/Vuelos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vuelos;
+
+/**
+ *
+ * @author Jonathan Saez
+ */
+public class Vuelos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
